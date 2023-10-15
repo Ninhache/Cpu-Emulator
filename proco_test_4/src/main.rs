7f25ddf752802189318da63055855e46f782679b
Ninhache <mr.carambar27@gmail.com>
2023-10-15 12:10:40 +0200

    fix: write as binary

--- proco_test_4/src/main.rs
+++ proco_test_4/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("AAAAAAAAAAAAA JEN AI MARRE");
+use std::fs::File;
+use std::io::prelude::*;
+
+
+fn main() -> std::io::Result<()> {
+    // https://stackoverflow.com/questions/53826371/how-to-create-a-binary-file-with-rust
+    {
+        let mut file = File::create("test")?;
+        file.write_all(&[0b01101101,0b00001101,0b10001101])?;
+    }
+
+    {
+        let mut file = File::open("test")?;
+        
+        let mut buffer = Vec::<u8>::new();
+        file.read_to_end(&mut buffer)?;
+    
+        buffer.iter().for_each(|part| {
+            println!("{:#010b}", part);
+        });
+        
+    }
+
+    Ok(())
 }
